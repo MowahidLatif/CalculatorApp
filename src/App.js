f5e3555745c590f244dadfcb195489920a673cf6
mowahidlatif <mowahidlatif@hotmail.com>
2022-11-23 14:43:14 -0600

    finished version css changes needed

--- src/App.js
+++ src/App.js
@@ -6,24 +6,79 @@ function App() {
   const numbers =[1,2,3,4,5,6,7,8,9]
   const operations = ['+', '-', '*', '/']
 
+  const [firstNumber, setFirstNumber] = useState("")
+  const [secondNumber, setSecondNumber] = useState("")
+  const [operation, setOperation] = useState("")
+  const [result, setResult] = useState(0)
+
+  useEffect(() => {
+    console.log(Number(firstNumber) + " " + operation + " " + Number(secondNumber));
+  }, [firstNumber, secondNumber, operation])
+
+  function selectNum(num) {
+    if (operation === "") {
+      setFirstNumber(firstNumber + num);
+    } else {
+      setSecondNumber(secondNumber + num);
+    }
+  }
+
+  function selectOp(op) {
+    setOperation(op);
+  }
+
+  function setZero(op) {
+    setResult(0);
+    setFirstNumber("");
+    setSecondNumber("");
+    setOperation("");
+
+  }
+
+  function computeResult() {
+    switch(operation) {
+      case '+':
+        setResult(Number(firstNumber) + Number(secondNumber));
+        break;
+      case '-':
+        setResult(Number(firstNumber) - Number(secondNumber));
+        break;
+      case '*':
+        setResult(Number(firstNumber) * Number(secondNumber));
+        break;
+      case '/':
+        setResult(Number(firstNumber) / Number(secondNumber));
+        break;
+      default:
+        setResult(0);
+    }
+  }
+
   return (
     <div className="App">
       <div className='calc'>
           <div className='numberSection'>
-            <div>Result</div>
+            <div>{result}</div>
           </div>
           <div className='operationSection'>
-            <div className='resultButton'>Result</div>
-            <div className='zeroButton'>Zero</div>
+            <div className='actionButton' onClick={()=>computeResult()}>Result</div>
+            <div className='actionButton' onClick={()=>setZero()}>Zero</div>
           </div>
           <div className='keypadSection'>
-            {
-              numbers.map((num) => {
+            <div className='squareNumbers'>
+              {numbers.map((num, key) => {
                 return (
-                  <div className='individualNumbers'>{num}</div>
+                  <div className='individualNumbers' onClick={() => selectNum(num)}>{num}</div>
                 );
-              })
-            }
+              })}
+            </div>
+            <div className='vertialOperations'>
+              {operations.map((op) => {
+                return (
+                  <div className='individualNumbers' onClick={() => selectOp(op)}>{op}</div>
+                );
+              })}
+            </div>
           </div>
       </div>
     </div>
